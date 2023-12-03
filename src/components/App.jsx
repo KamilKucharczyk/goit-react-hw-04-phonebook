7040e6364901254ef653286781cf14ed0905e258
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2023-12-03 17:17:35 +0100

    Src files replaced correct

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,19 @@ export class App extends Component {
     number: '',
   };
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContactOnSubmit = ({ name, number }) => {
     const contactOnList = this.state.contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase()
